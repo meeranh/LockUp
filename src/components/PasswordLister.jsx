414aa9c9637f6b52a9c050dd6ec7a3c77bf72910
meeranh <themeeranhassan@gmail.com>
2023-02-12 11:54:36 +0530

    Passwords hidden by default

--- src/components/PasswordLister.jsx
+++ src/components/PasswordLister.jsx
@@ -3,13 +3,22 @@ import PassContext from './contexts/PassContext';
 
 const PasswordLister = ({ email, password, id }) => {
   const { deletePassword } = useContext(PassContext);
-  console.log(id);
+
+  const togglePassword = (e) => {
+    if (e.target.getAttribute('reveal') === 'false') {
+      e.target.innerHTML = e.target.getAttribute('password');
+      e.target.setAttribute('reveal', 'true');
+    } else {
+      e.target.innerHTML = '******';
+      e.target.setAttribute('reveal', 'false');
+    }
+  }
 
   return (
     <>
       <tr>
         <td>{email}</td>
-        <td>{password}</td>
+        <td password={password} reveal='false' onClick={(e) => {togglePassword(e)}}>******</td>
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
           <button onClick={() => navigator.clipboard.writeText(password)}>Copy</button>
           <button onClick={() => deletePassword(id)}>Delete</button>
