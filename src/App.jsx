a6db0792f3600e51b445a1ff3ede7a3f531af4f5
meeranh <themeeranhassan@gmail.com>
2023-02-18 21:24:43 +0530

    Centered control buttons

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import TopButtons from './components/TopButtons';
 import MainUI from './components/MainUI';
 import RequestMasterPasswordUI from './components/dataInputUI/RequestMasterPasswordUI';
 import { PassProvider } from './components/contexts/PassContext';
+import SmallScreenWarning from './components/SmallScreenWarning';
 
 function App() {
   return (
@@ -11,11 +12,7 @@ function App() {
         <TopButtons />
         <br />
       </header>
-      <div className="md:hidden px-8">
-        <p className="warning text-center font-bold text-white text-sm bg-gray-800 shadow-md rounded pt-3 pb-4 mb-4">
-          Use a laptop or a desktop for a better experience
-        </p>
-      </div>
+    <SmallScreenWarning/>
       <MainUI />
     </PassProvider >
   )
--- src/components/TopButtons.jsx
+++ src/components/TopButtons.jsx
@@ -10,16 +10,16 @@ const TopButtons = () => {
         <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white text-center mx-auto">LockUp Password Manager</span>
         <div className="md:block md:w-auto sm:flex mx-auto">
           <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
+            <li className="mx-auto">
               <ExportBtn />
             </li>
-            <li>
+            <li className="mx-auto">
               <SaveLocalBtn />
             </li>
-            <li>
+            <li className="mx-auto">
               <ImportBtn />
             </li>
-            <li>
+            <li className="mx-auto">
               <LoadLocalBtn />
             </li>
           </ul>
