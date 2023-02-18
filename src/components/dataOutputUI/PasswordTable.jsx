20004d6700470a01f8767e9018fbaa22e6cff9d0
meeranh <themeeranhassan@gmail.com>
2023-02-18 21:03:00 +0530

    Edited table column name

--- src/components/dataOutputUI/PasswordTable.jsx
+++ src/components/dataOutputUI/PasswordTable.jsx
@@ -7,7 +7,7 @@ const PasswordTable = ({ data }) => {
       <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
-            <th scope="col" className="px-6 py-3">Email</th>
+            <th scope="col" className="px-6 py-3">Email / Username</th>
             <th scope="col" className="px-6 py-3 text-center">Password (Click to reveal)</th>
             <th scope="col" className="px-6 py-3 text-right">Actions</th>
           </tr>
