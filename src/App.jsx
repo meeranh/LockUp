ac80f6c241cfde5b6309eb6a9a2b958e50455583
meeranh <themeeranhassan@gmail.com>
2023-02-18 18:49:35 +0530

    Added screen-width warning

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,13 @@ function App() {
         <TopButtons />
         <br />
       </header>
+      <div className="md:hidden">
+        <p className="warning text-center font-bold text-white text-sm bg-gray-800 shadow-md rounded pt-3 pb-4 mb-4">
+          Use a laptop or a desktop for a better experience
+        </p>
+      </div>
       <MainUI />
-    </PassProvider>
+    </PassProvider >
   )
 }
 
