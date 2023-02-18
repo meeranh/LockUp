b0e22d343ab7b93fa5852388fd1f812c761ef4fe
meeranh <themeeranhassan@gmail.com>
2023-02-18 19:09:23 +0530

    Fixed typo and adjusted padding

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
         <TopButtons />
         <br />
       </header>
-      <div className="md:hidden">
+      <div className="md:hidden px-8">
         <p className="warning text-center font-bold text-white text-sm bg-gray-800 shadow-md rounded pt-3 pb-4 mb-4">
           Use a laptop or a desktop for a better experience
         </p>
