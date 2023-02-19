c4996391620101b8b22fe034223b7c420e518d56
meeranh <themeeranhassan@gmail.com>
2023-02-19 07:37:46 +0530

    Made UI responsive

--- src/components/TopButtons.jsx
+++ src/components/TopButtons.jsx
@@ -8,18 +8,18 @@ const TopButtons = () => {
     <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
       <div className="container flex flex-wrap items-center justify-between mx-auto">
         <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white text-center mx-auto">LockUp Password Manager</span>
-        <div className="md:block md:w-auto sm:flex mx-auto">
-          <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li className="mx-auto">
+        <div className="mx-auto">
+          <ul className="flex items-end flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <li className="">
               <ExportBtn />
             </li>
-            <li className="mx-auto">
+            <li className="">
               <SaveLocalBtn />
             </li>
-            <li className="mx-auto">
+            <li className="">
               <ImportBtn />
             </li>
-            <li className="mx-auto">
+            <li className="">
               <LoadLocalBtn />
             </li>
           </ul>
--- src/components/dataOutputUI/PasswordTable.jsx
+++ src/components/dataOutputUI/PasswordTable.jsx
@@ -3,8 +3,8 @@ import PasswordLister from './PasswordLister';
 // An array of emails and passwords will be taken as props and passed onto PasswordLister for iteration
 const PasswordTable = ({ data }) => {
   return (
-    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+    <div className="relative shadow-md sm:rounded-lg">
+      <table className="md:w-full text-sm text-left text-gray-500 dark:text-gray-400 w-2/3 mx-auto">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
             <th scope="col" className="px-6 py-3">Email / Username</th>
